2621eb55f0b9877bc1c211bb721881db26079c9f
Pavel Alekseev <pavelbooking.com@yandex.ru>
2023-01-31 13:29:19 +0500

    express-mesto-gha

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ const { PORT = 3000 } = process.env;
 const DATA_URL = 'mongodb://127.0.0.1:27017/mestodb';
 
 app.use(cookieParser());
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -43,7 +44,7 @@ app.get('/signout', (req, res) => {
 app.use('/users', auth, routerUser);
 app.use('/cards', auth, routerCard);
 app.use('*', auth, (req, res, next) => {
-  next(new NotFound('Страница не существует'));
+  next(new NotFound('Страница не найдена'));
 });
 
 app.use(errors());
