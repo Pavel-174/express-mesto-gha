874e876a330fca53c82ef856e0f6c668732389aa
Pavel Alekseev <pavelbooking.com@yandex.ru>
2023-01-31 05:01:01 +0500

    express-mesto-gha

--- controllers/users.js
+++ controllers/users.js
@@ -1,11 +1,7 @@
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const User = require('../models/user');
-const {
-  NotFound, ValidationError, ConflictError, AuthError,
-} = require('../errors/index');
-
-const { NODE_ENV, JWT_SECRET } = process.env;
+const { NotFound, ValidationError, ConflictError } = require('../errors/index');
 
 const getUsers = (req, res, next) => {
   User.find({})
@@ -34,20 +30,33 @@ const getUser = (req, res, next) => {
 
 const createUser = (req, res, next) => {
   const {
-    name, about, avatar, email,
+    name,
+    about,
+    avatar,
+    email,
+    password,
   } = req.body;
-  bcrypt.hash(req.body.password, 10)
-    .then((hash) => {
-      User.create({
-        name, about, avatar, email, password: hash,
-      });
-    })
-    .then((user) => res.status(201).send(user))
-    .catch((err) => {
-      if (err.name === 'ValidationError') {
-        throw new ValidationError('Переданы некорректные данные при создании пользователя');
-      } else if (err.code === 11000) {
-        throw new ConflictError('Пользователь с таким email уже существует');
+
+  User.findOne({ email })
+    .then((mail) => {
+      if (mail) {
+        throw new ConflictError('Пользователь с таким email уже существует.');
+      } else {
+        bcrypt.hash(password, 10)
+          .then((hash) => User.create({
+            name,
+            about,
+            avatar,
+            email,
+            password: hash,
+          }))
+          .then((user) => res.status(200).send(user))
+          .catch((err) => {
+            if (err.name === 'ValidationError') {
+              throw new ValidationError('Переданы некорректные данные при создании пользователя.');
+            }
+          })
+          .catch(next);
       }
     })
     .catch(next);
@@ -109,23 +118,11 @@ const getCurrentUser = (req, res, next) => {
 
 const login = (req, res, next) => {
   const { email, password } = req.body;
-  User.findOne({ email }).select('+password')
+
+  return User.findUserByCredentials(email, password)
     .then((user) => {
-      if (user === null) {
-        throw new AuthError('Неправильная почта или пароль');
-      } return bcrypt.compare(password, user.password)
-        .then((matched) => {
-          if (!matched) {
-            throw new AuthError('Неправильная почта или пароль');
-          } const token = jwt.sign({ _id: user._id }, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret', { expiresIn: '7d' });
-          res.cookie('jwt', token, { maxAge: 3600000 * 7, httpOnly: true, sameSite: true }).send({
-            name: user.name,
-            about: user.about,
-            avatar: user.avatar,
-            email: user.email,
-            _id: user._id,
-          });
-        });
+      const token = jwt.sign({ _id: user._id }, 'yandex-praktikum', { expiresIn: '7d' });
+      res.send({ token });
     })
     .catch(next);
 };
